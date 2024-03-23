a045f659dd909195360e989dc6ea533e309a002d
Ben3940 <bty2015@comcast.net>
2024-03-23 18:15:47 -0400

    Included canvas boundries to cast rays at

--- Ray_Casting/main.js
+++ Ray_Casting/main.js
@@ -1,5 +1,5 @@
-const DIM = 500;
-const NUM_WALLS = 5;
+const DIM = 800;
+const NUM_WALLS = 10;
 let walls = [];
 let particle;
 
@@ -12,6 +12,18 @@ function setup() {
     const y2 = random(height);
     walls.push(new Wall(x1, y1, x2, y2));
   }
+  // top wall
+  walls.push(new Wall(0, 0, width, 0));
+
+  // right wall
+  walls.push(new Wall(width, 0, width, height));
+
+  // left wall
+  walls.push(new Wall(0, 0, 0, height));
+
+  // bottom wall
+  walls.push(new Wall(0, height, width, height));
+
   particle = new Particle(5);
 }
 
