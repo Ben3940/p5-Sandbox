32e7c42448c558bb869a5d44317c0a1b7fdddf7e
Ben3940 <bty2015@comcast.net>
2024-03-25 17:33:15 -0400

    Added FOV aspect to particle's rays to give narrow impression of 'beam' of light from particle

--- Ray_Casting/src/Particle.js
+++ Ray_Casting/src/Particle.js
@@ -6,18 +6,31 @@
 
 class Particle {
   // Initializes rays array with a Ray object every angle_incr up to 360 degrees
-  constructor(angle_incr = 10, speed = 5) {
+  constructor(
+    angle_incr = 5,
+    speed = 5,
+    FOV = 0,
+    angle_offset = 0,
+    rotate_speed = 4
+  ) {
     this.pos = createVector(width / 2, height / 2);
     this.rays = [];
+    this.angle_incr = angle_incr;
     this.speed = speed;
+    this.FOV = FOV;
+    this.angle_offset = angle_offset;
+    this.rotate_speed = rotate_speed;
 
-    for (let angle = 0; angle < 360; angle += angle_incr) {
-      this.rays.push(new Ray(this.pos, radians(angle)));
+    if (this.FOV) {
+      this.generate_rays_FOV();
+    } else {
+      this.generate_rays();
     }
   }
 
   // Iterate rays, determine which wall is closest to array and cast ray at wall
   cast(walls) {
+    stroke(0, 255, 0);
     this.rays.forEach((ray) => {
       let closest = null;
       let min_pnt = Infinity;
@@ -43,27 +56,43 @@ class Particle {
 
   // Check key(s) pressed and move particle
   move() {
-    if (keyIsDown(87) && keyIsDown(65)) {
-      particle.moveUp();
-      particle.moveLeft();
-    } else if (keyIsDown(87) && keyIsDown(68)) {
-      particle.moveUp();
-      particle.moveRight();
-    } else if (keyIsDown(83) && keyIsDown(65)) {
-      particle.moveDown();
-      particle.moveLeft();
-    } else if (keyIsDown(83) && keyIsDown(68)) {
-      particle.moveDown();
-      particle.moveRight();
-    } else if (keyIsDown(65)) {
-      particle.moveLeft();
-    } else if (keyIsDown(68)) {
-      particle.moveRight();
-    } else if (keyIsDown(87)) {
-      particle.moveUp();
-    } else if (keyIsDown(83)) {
-      particle.moveDown();
+    if (keyIsDown(37)) {
+      this.rotateLeft();
+      this.generate_rays_FOV();
+    } else if (keyIsDown(39)) {
+      this.rotateRight();
+      this.generate_rays_FOV();
     }
+
+    if (keyIsDown(87) && keyIsDown(65)) {
+      this.moveUp();
+      this.moveLeft();
+    } else if (keyIsDown(87) && keyIsDown(68)) {
+      this.moveUp();
+      this.moveRight();
+    } else if (keyIsDown(83) && keyIsDown(65)) {
+      this.moveDown();
+      this.moveLeft();
+    } else if (keyIsDown(83) && keyIsDown(68)) {
+      this.moveDown();
+      this.moveRight();
+    } else if (keyIsDown(65)) {
+      this.moveLeft();
+    } else if (keyIsDown(68)) {
+      this.moveRight();
+    } else if (keyIsDown(87)) {
+      this.moveUp();
+    } else if (keyIsDown(83)) {
+      this.moveDown();
+    }
+  }
+
+  rotateLeft() {
+    this.angle_offset -= this.rotate_speed;
+  }
+
+  rotateRight() {
+    this.angle_offset += this.rotate_speed;
   }
 
   // Move particle in specified direction.  max/min are used to bound particle to canvas
@@ -86,5 +115,22 @@ class Particle {
   // Render ray from current (x, y) to (x2, y2)
   render(x2, y2) {
     line(this.pos.x, this.pos.y, x2, y2);
+  }
+
+  generate_rays() {
+    for (let angle = 0; angle <= 360; angle += this.angle_incr) {
+      this.rays.push(new Ray(this.pos, radians(angle)));
+    }
+  }
+
+  generate_rays_FOV() {
+    this.rays = [];
+    for (
+      let angle = -this.FOV + this.angle_offset;
+      angle < this.FOV + this.angle_offset;
+      angle += this.angle_incr
+    ) {
+      this.rays.push(new Ray(this.pos, radians(angle)));
+    }
   }
 }
