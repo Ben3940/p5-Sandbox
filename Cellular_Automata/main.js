4d28a094e7f60c44a4bb962387ffdc4ec1def232
Ben3940 <bty2015@comcast.net>
2024-04-04 21:21:52 -0400

    Created initial 1D cellular automata state.  Still need rules for automata to follow

--- Cellular_Automata/main.js
+++ Cellular_Automata/main.js
@@ -1,9 +1,15 @@
-const DIM = 500;
-
+const DIM = 400;
+const cells = [0, 1, 0, 1, 1, 0, 0, 1];
+const w = DIM / 10;
 function setup() {
   createCanvas(DIM, DIM);
 }
 
 function draw() {
   background(0);
+  for (let i = 0; i < cells.length; i++) {
+    stroke(255);
+    fill(255 * cells[i]);
+    rect(i * w, 0, i * w, w);
+  }
 }
