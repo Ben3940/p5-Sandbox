2159fcb662eceecccd3927e614050ede5bcfe1fd
Ben3940 <bty2015@comcast.net>
2024-04-06 14:25:18 -0400

    Added input field for user to specify which rule to apply to cellular automata

--- Cellular_Automata/main.js
+++ Cellular_Automata/main.js
@@ -1,14 +1,12 @@
-const DIM = 600;
+const DIM = 900;
 let cells = [];
 const w = 10;
-let y = 0;
-const rulesets = {
-  178: [1, 0, 1, 1, 0, 1, 1, 0],
-};
+let y;
+let ruleset;
 function setup() {
-  createCanvas(DIM, DIM);
-  background(0);
-  populate_cells();
+  createCanvas(DIM, DIM * 3);
+  initialize_canvas();
+  generate_rule_set();
 }
 
 function draw() {
@@ -21,10 +19,16 @@ function draw() {
   update_cells();
 }
 
+function initialize_canvas() {
+  background(0);
+  populate_cells();
+  y = 0;
+}
+
 function populate_cells() {
   total_cells = width / w;
   for (let i = 0; i < total_cells; i++) {
-    if (i === floor(total_cells / 2)) {
+    if (i === floor(total_cells / 2) - 2) {
       cells[i] = 1;
     } else {
       cells[i] = 0;
@@ -33,16 +37,37 @@ function populate_cells() {
 }
 
 function update_cells() {
-  let next_cells = [cells[0]];
+  let temp_seq = cells.slice(-1).concat(cells.slice(0, 2));
+  let next_cells = [apply_rule_set(temp_seq)];
   for (let i = 1; i < cells.length - 1; i++) {
     const seq = cells.slice(i - 1, i + 2);
-    next_cells.push(rule_set(178, seq));
+    next_cells.push(apply_rule_set(seq));
   }
-  next_cells.push(cells[-1]);
+  temp_seq = cells.slice(-2).concat(cells[0]);
+
+  next_cells.push(apply_rule_set(temp_seq));
   cells = next_cells;
 }
 
-function rule_set(rule, seq) {
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  generate_rule_set();
+  initialize_canvas();
+});
+
+function generate_rule_set() {
+  const binary_rule = Number(document.querySelector('#ruleset').value)
+    .toString(2)
+    .padStart(8, '0');
+  console.log(binary_rule);
+  let rule = [];
+  binary_rule.split('').forEach((elm) => {
+    rule.push(Number(elm));
+  });
+  ruleset = rule;
+}
+
+function apply_rule_set(seq) {
   let value = 7 - parseInt(seq.join(''), 2);
-  return rulesets[rule][value];
+  return ruleset[value];
 }
