8a70800a6c31535856a6767285d76733e78296b8
Ben3940 <bty2015@comcast.net>
2024-04-09 18:54:36 -0400

    Updated README to include description of demo and underlying math behind generating rulesets

--- Cellular_Automata/README.md
+++ Cellular_Automata/README.md
@@ -12,6 +12,30 @@ The facination with these Automatas is that the rulesets can seem random/chaotic
 
 ### How is a Ruleset Generated
 
-Rulesets revolve around the idea of base 2 (binary) numbers. They also consider a sequence of 3 consecutive cell states (0 or 1) to determine the new cell state. Since a cell can only have a state of either 0 or 1, that means there are 2 _ 2 _ 2 = 8 different sequences (i.e. 000, 001, 010, etc.). This works well with binary numbers, where each bit can be either 0 or 1, meaning we only need 8 bits to map all 8 different sequences to a state of either 0 or 1.
+Rulesets revolve around the idea of base 2 (binary) numbers. They also consider a sequence of 3 consecutive cell states (0 or 1) to determine the new cell state. Since a cell can only have a state of either 0 or 1, that means there are 2 x 2 x 2 = 8 different sequences (i.e. 000, 001, 010, etc.). This works well with binary numbers, where each bit can be either 0 or 1, meaning we only need 8 bits to map all 8 different sequences to a state of either 0 or 1.
 
 A 8-bit binary number caps at 256 (in base 10) different binary numbers. Therefore, converting any base 10 number from 0 to 255 will result in a ruleset that is just the base 2 representation of that base 10 number.
+
+### Example:
+
+Rule 30 takes the decimal value 30 and converts it to the binary representation.
+
+    30 (decimal) = 00011110 (binary)
+
+Remember this is an 8-bit binary number so each of the 8 unique sequences of 3 consecutive cell states is mapped to a single bit. So starting from the left-most bit and the sequence of '111' the bits are mapped as followed:
+
+    Sequence | New State | Array Index
+      '111'  |    '0'    |      0
+      '110'  |    '0'    |      1
+      '101'  |    '0'    |      2
+      '100'  |    '1'    |      3
+      '011'  |    '1'    |      4
+      '010'  |    '1'    |      5
+      '001'  |    '1'    |      6
+      '000'  |    '0'    |      7
+
+NOTE: '111' is 7 in binary. In the apply_rule_set() the line
+
+    const value = 7 - parseInt(seq.join(''), 2);
+
+Reverses the index value since it is subtracting the sequence value ('111' or 7 in this case) from 7. So '111' corresponds with index 0 for the array that stores the individual bits of the binary number.
--- Cellular_Automata/main.js
+++ Cellular_Automata/main.js
@@ -12,7 +12,8 @@ function setup() {
 function draw() {
   for (let i = 0; i < cells.length; i++) {
     stroke(0);
-    // fill(255 * cells[i]);
+
+    // If cell exists, render colored cell. otherwise cell is black
     if (cells[i][0]) {
       fill(cells[i][1]['R'], cells[i][1]['G'], cells[i][1]['B']);
     } else {
@@ -20,16 +21,19 @@ function draw() {
     }
     square(i * w, y, w);
   }
+  // Move to next row and update cell array
   y += w;
   update_cells();
 }
 
+// Clear background, re-populate cells array, start at first row
 function initialize_canvas() {
   background(0);
   populate_cells();
   y = 0;
 }
 
+// Iterate across row of cells, setting middle middle as only active cell
 function populate_cells() {
   total_cells = width / w;
   for (let i = 0; i < total_cells; i++) {
@@ -41,44 +45,59 @@ function populate_cells() {
   }
 }
 
+// Determine new cell state based on 3 consecutive cells from row above
 function update_cells() {
   const len = cells.length;
   let next_cells = [];
   for (let i = 0; i < len; i++) {
+    // Modulo allows for wrapping around on edge cells (i.e. i = 0 & i = len - 1)
     const left = cells[(i - 1 + len) % len][0];
     const middle = cells[i][0];
     const right = cells[(i + 1 + len) % len][0];
     next_cells.push(apply_rule_set([left, middle, right]));
   }
+  // New cell states are set to cells as next generation (row) of cells
   cells = next_cells;
 }
 
+// Submission of form lets user define ruleset number and re-initializes canvas with new ruleset
 document.querySelector('#form').addEventListener('submit', (e) => {
   e.preventDefault();
   generate_rule_set();
   initialize_canvas();
 });
 
+// Grab integer value from HTML input ("#ruleset") and generate ruleset based on this value
 function generate_rule_set() {
+  // Convert value as Number, then to string as base-2 number, add '0' padding incase first
+  //   few bits are 0 (i.e. 30 => 00011110  JS will drop first 3 '0' from left side)
   const binary_rule = Number(document.querySelector('#ruleset').value)
     .toString(2)
     .padStart(8, '0');
-  console.log(binary_rule);
+
   let rule = [];
+  // Split binary_rule string into individual bits, convert them to Numbers and add to array
   binary_rule.split('').forEach((elm) => {
     rule.push(Number(elm));
   });
+
   ruleset = rule;
 }
 
+// Consider sequence of 3 cells and generate new state based on these cells' states
 function apply_rule_set(seq) {
+  // Interpret sequence array as binary number and parse the decimal value of that number
+  //  Subtract from 7 to invert ordering (see README)
   const value = 7 - parseInt(seq.join(''), 2);
   const cell_state = ruleset[value];
+
   let cell_color = assign_color(value);
 
+  // New cell has a state (0 or 1) and color based on what rule determined this state
   return [cell_state, cell_color];
 }
 
+// Given ruleset index value, assign a color for cell
 function assign_color(value) {
   let cell_color = {
     R: 0,
