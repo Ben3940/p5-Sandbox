0f10a4a11eae08caa633239b97fcf56e52863feb
Ben3940 <bty2015@comcast.net>
2024-04-17 19:10:41 -0400

    Created hilbert function to return ith point along hilbert first-order curve

--- Hilbert_Curve/main.js
+++ Hilbert_Curve/main.js
@@ -1,9 +1,27 @@
 const DIM = 500;
+const ORDER = 1;
+const N = 2 ^ ORDER;
+const TOTAL = N ^ 2;
+const PATH = [];
 
 function setup() {
   createCanvas(DIM, DIM);
+
+  for (let i = 0; i < TOTAL; i++) {
+    vec = hilbert_x_y(i);
+  }
 }
 
 function draw() {
   background(0);
+}
+
+function hilbert_x_y(i) {
+  const points = [
+    createVector(0, 0),
+    createVector(0, 1),
+    createVector(1, 1),
+    createVector(1, 0),
+  ];
+  return points[i];
 }
