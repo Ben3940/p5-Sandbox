6778c91bff7b56ef5f83681960f55894b8bc117c
Ben3940 <bty2015@comcast.net>
2024-05-07 17:30:53 -0400

    Added function to generate sine values and another to render the final sine curve

--- GA_Sin_Cos_Curves/main.js
+++ GA_Sin_Cos_Curves/main.js
@@ -1,4 +1,4 @@
-const DIM = 700;
+const DIM = 500;
 const OFFSET = DIM / 10;
 
 function setup() {
@@ -19,6 +19,31 @@ function draw_y_axis() {
 function draw_x_axis() {
   stroke(255);
   line(OFFSET, height / 2, width, height / 2);
+  draw_sine(DIM / 100);
 }
 
-function draw_sine() {}
+function draw_sine(step_size) {
+  strokeWeight(5);
+  stroke('purple');
+  const n_samples = floor((width - OFFSET) / step_size);
+  const range = compute_sine(n_samples);
+  for (let i = 0; i < range.length; i++) {
+    const x = i * step_size + OFFSET;
+    const y = range[i];
+    console.log(y);
+    point(x, y);
+  }
+  stroke(255);
+  strokeWeight(1);
+}
+
+function compute_sine(n_samples) {
+  const step_size = (2 * PI) / n_samples;
+  let range_sine = [];
+  for (let i = 0; i < 2 * PI; i += step_size) {
+    const y = sin(i);
+    const y_map = map(y, -1, 1, 0, height);
+    range_sine.push(y_map);
+  }
+  return range_sine;
+}
