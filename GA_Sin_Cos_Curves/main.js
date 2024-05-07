21545a1fc3b93270e70ca062e61fb3d0244be731
Ben3940 <bty2015@comcast.net>
2024-05-07 19:37:24 -0400

    Created more generic function that takes either 'sin' or 'cos' as function argument and to call within compute_curve to generate y values

--- GA_Sin_Cos_Curves/main.js
+++ GA_Sin_Cos_Curves/main.js
@@ -26,7 +26,7 @@ function draw_sine(step_size) {
   strokeWeight(5);
   stroke('purple');
   const n_samples = floor((width - OFFSET) / step_size);
-  const range = compute_sine(n_samples);
+  const range = compute_curve(n_samples, sin);
   for (let i = 0; i < range.length; i++) {
     const x = i * step_size + OFFSET;
     const y = range[i];
@@ -37,12 +37,12 @@ function draw_sine(step_size) {
   strokeWeight(1);
 }
 
-function compute_sine(n_samples) {
+function compute_curve(n_samples, curve_func) {
   const step_size = (2 * PI) / n_samples;
   let range_sine = [];
   for (let i = 0; i < 2 * PI; i += step_size) {
-    const y = sin(i);
-    const y_map = map(y, -1, 1, 0, height);
+    const y = curve_func(i);
+    const y_map = map(y, -1, 1, height, 0);
     range_sine.push(y_map);
   }
   return range_sine;
