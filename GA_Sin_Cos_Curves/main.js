75e328d7af174bfac0057987e1cbedf815bc4455
Ben3940 <bty2015@comcast.net>
2024-05-07 17:07:33 -0400

    Added functions to draw lines for x and y axes

--- GA_Sin_Cos_Curves/main.js
+++ GA_Sin_Cos_Curves/main.js
@@ -1,4 +1,5 @@
-const DIM = 500;
+const DIM = 700;
+const OFFSET = DIM / 10;
 
 function setup() {
   createCanvas(DIM, DIM);
@@ -6,4 +7,18 @@ function setup() {
 
 function draw() {
   background(0);
+  draw_y_axis();
+  draw_x_axis();
 }
+
+function draw_y_axis() {
+  stroke(255);
+  line(OFFSET, 0, OFFSET, height);
+}
+
+function draw_x_axis() {
+  stroke(255);
+  line(OFFSET, height / 2, width, height / 2);
+}
+
+function draw_sine() {}
