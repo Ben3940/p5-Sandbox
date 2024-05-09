aa3496cafe4bf641c9f460fdd57d90ea5ceab577
Ben3940 <bty2015@comcast.net>
2024-05-09 15:46:23 -0400

    Created function to generate a selection pool.  Will actually need to re-evaluate program structure, code is becoming too complex.

--- GA_Sin_Cos_Curves/src/GA.js
+++ GA_Sin_Cos_Curves/src/GA.js
@@ -1,10 +1,12 @@
 class GA {
   best_candidate;
+  selection_pool = [];
+  population = [];
   constructor(DIM, n_samples, size, max_value, min_value, true_values) {
     this.DIM = DIM;
     this.n_samples = n_samples;
     this.pop_size = size;
-    this.population = [];
+
     this.max_value = max_value;
     this.min_value = min_value;
     this.true_values = true_values;
@@ -35,9 +37,8 @@ class GA {
   }
 
   fitness() {
-    let fitness_scores = [];
     let best_score = 1000;
-
+    let total_score = 0;
     this.population.forEach((candidate) => {
       let fitness_score = 0;
       for (let i = 0; i < TRUE_VALUES.length; i++) {
@@ -47,8 +48,34 @@ class GA {
       if (fitness_score < best_score) {
         this.best_candidate = candidate;
       }
-      fitness_scores.push(fitness_score);
+      total_score += fitness_score;
+      this.selection_pool.push([fitness_score, candidate]);
     });
-    return fitness_scores;
+    return total_score;
+  }
+
+  generate_pool(total_score){
+    let marker = 0;
+    let pool = [];
+
+    this.selection_pool(candidate => {
+      const score = candidate[0];
+      const instances = Math.round(score/total_score, 2) * 100;
+      pool.push([marker, marker + instances, candidate[1]]);
+    })
+
+    for (let n = 0; n < this.pop_size; n++){
+      const rand = Math.random() * 100;
+      for (let i = 0; i< pool.length; i++){
+        const cand = pool[i];
+        if (rand >= cand[0] && rand <= cand[1]){
+          
+        }
+      }
+    }
+  }
+
+  selection() {
+    this.selection_pool{}
   }
 }
